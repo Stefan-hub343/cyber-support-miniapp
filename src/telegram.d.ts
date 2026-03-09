4ebef9d1cd97c8daacb6012ba0308aa9ea3b2e73
Stefan <stefanbudimir05@gmail.com>
2026-03-09 07:45:00 +0300

    Добавлена мини-игра, исправлены типы Telegram

--- src/telegram.d.ts
+++ src/telegram.d.ts
@@ -11,13 +11,18 @@ declare global {
           hide: () => void;
           onClick: (callback: () => void) => void;
         };
+        initData: string;  // Добавлено!
         initDataUnsafe?: {
+          query_id?: string;
           user?: {
             id: number;
             first_name: string;
             last_name?: string;
             username?: string;
+            language_code?: string;
           };
+          auth_date?: number;
+          hash?: string;
         };
       };
     };
